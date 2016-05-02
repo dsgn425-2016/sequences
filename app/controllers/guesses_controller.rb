06ab6ada2dde844fe59046535e429bf8ac55a73a
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-05-02 17:49:52 -0500

    User entry of numbers possible

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,6 +1,11 @@
 class GuessesController < ApplicationController
 
   def index
+    #Parameters: {"first_number"=>"1", "second_number"=>"2", "third_number"=>"3"} - rails is doing this
+
+    @user_first = params[:first_number]
+    @user_second = params[:second_number]
+    @user_third = params[:third_number]
 
     render("guesses.html.erb")
   end
