d0abdaf27ff10cbf5b690b2c9f0a0064bc921402
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-05-02 18:17:41 -0500

    Using databases in rails

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -3,9 +3,13 @@ class GuessesController < ApplicationController
   def index
     #Parameters: {"first_number"=>"1", "second_number"=>"2", "third_number"=>"3"} - rails is doing this
 
-    @user_first = params[:first_number]
-    @user_second = params[:second_number]
-    @user_third = params[:third_number]
+    g = Guess.new
+    g.first = params[:first_number]
+    g.second = params[:second_number]
+    g.third = params[:third_number]
+    g.save
+
+    @list_of_guesses = Guess.all
 
     render("guesses.html.erb")
   end
