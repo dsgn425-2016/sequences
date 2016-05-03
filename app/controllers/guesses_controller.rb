de0e602b46005817b82a9b9ab7623c7304f988a9
Anita Prasad <aprasad2017@kellogg.northwestern.edu>
2016-05-02 22:36:16 -0500

    Anita Prasad Sequences
    First upload from class

---
+++ app/controllers/guesses_controller.rb
@@ -0,0 +1,23 @@
+class GuessesController < ApplicationController
+  def create_row
+    g = Guess.new
+    g.first = params[:first_number]
+    g.second = params[:second_number]
+    g.third = params[:third_number]
+    g.save
+
+    redirect_to("http://localhost:3000/all_guesses")
+  end
+
+  def index
+
+    @list_of_guesses = Guess.all
+
+    @first_num = params["first_number"]
+    @second_num = params["second_number"]
+    @third_num = params["third_number"]
+
+    render ("index.html.erb")
+
+  end
+end
