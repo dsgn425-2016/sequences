4e4449dd2c3c225cd373c55b9fa4f8344264345a
StanleyTack <stanley.tack@users.noreply.github.com>
2016-05-07 13:08:17 -0500

    new

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,5 +1,18 @@
 class GuessesController <ApplicationController
   def index
+
+    g=Guess.new
+    g.first = params[:first_number]
+    g.second = params[:second_number]
+    g.third = params[:third_number]
+    g.save
+
+
+    Guess.all.each do |guess|
+      @result = "No."
+      @result = "Yes!"
+    end
+
     render("index.html.erb")
   end
 end
