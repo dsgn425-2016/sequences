0c8dac5bd68d68479cec214a9310528d72d426fc
sof-am <sofiamachline1@air-de-sofia.attlocal.net>
2016-05-08 13:54:45 -0500

    found a difference - still doesn't work

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@
 ActiveRecord::Schema.define(version: 20160502230544) do
 
   create_table "guesses", force: :cascade do |t|
+    t.integer  "first"
+    t.integer  "second"
+    t.integer  "third"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
